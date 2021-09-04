709f8ffb7438b487ead674153275c98abdd435dd
paolo.carraro <paolo.carraro@interlogica.it>
2021-09-05 00:16:01 +0200

    Server with type handler

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"net/http"
+)
+
+type srv string
+
+func (s srv) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/metal":
+		w.Write([]byte("sei metal"))
+	case "/dark":
+		w.Write([]byte("sei dark"))
+	case "/pop":
+		w.Write([]byte("sei pop"))
+	}
+}
+
 func main() {
+	var s srv
+
+	http.ListenAndServe(":8080", s)
 
 }
