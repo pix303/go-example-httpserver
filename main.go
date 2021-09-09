0ac4c426bb71e1822a0705513ce6e8a6c4913b19
paolo.carraro <paolo.carraro@interlogica.it>
2021-09-09 23:39:21 +0200

    Update with 2 different middleware func

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Genre struct {
@@ -24,7 +24,6 @@ func metalHandler(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	fmt.Println(metal)
 	result, err := json.Marshal(metal)
 	if err != nil {
 		http.Error(w, "Error on json", 500)
@@ -32,7 +31,24 @@ func metalHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(result))
 }
 
+var start time.Time
+
+func logRequest(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start = time.Now()
+		log.Printf("Request on process: %s - %s\r", r.Method, r.URL.Path)
+		handler.ServeHTTP(w, r)
+	}
+}
+
+func logResponse(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+		log.Printf("Request processed in %d\r", time.Since(start).Milliseconds())
+	}
+}
+
 func main() {
-	http.HandleFunc("/metal", metalHandler)
+	http.HandleFunc("/metal", logRequest(logResponse(metalHandler)))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
