3915e20a29b00ce82ae2d69e4e593b903c2d3d76
paolo.carraro <paolo.carraro@interlogica.it>
2021-10-25 10:58:39 +0200

    Add auth middleware with params

--- main.go
+++ main.go
@@ -51,7 +51,43 @@ func genreHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(result))
 }
 
+func addGenreHandler(w http.ResponseWriter, r *http.Request) {
+
+	method := r.Method
+	if method != http.MethodPost {
+		http.Error(w, "Method not accepted", http.StatusMethodNotAllowed)
+		return
+	}
+	// var item []byte
+	// itemSize, err := r.Body.Read(item)
+	// defer r.Body.Close()
+
+	var g Genre
+	err := json.NewDecoder(r.Body).Decode(&g)
+	if err != nil {
+		http.Error(w, "Error on read body request", http.StatusBadRequest)
+		return
+	}
+	log.Println(g)
+
+	w.Write([]byte("Genre added"))
+}
+
+func logHeaders(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("---Headers for %s\r", r.URL.Path)
+		for k, v := range r.Header {
+			log.Println(k, v)
+		}
+		log.Printf("---Headers-------------end-----\n\n")
+		h.ServeHTTP(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/genre", genreHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/genre", logHeaders(genreHandler))
+	http.HandleFunc("/genre/add", logHeaders(addGenreHandler))
+	log.Println("Server running...")
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
 }
